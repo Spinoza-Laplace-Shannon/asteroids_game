4a4c06569e3b45206521c5532d667fcb467016c7
Spinoza-Laplace-Shannon <156420933+Spinoza-Laplace-Shannon@users.noreply.github.com>
2026-03-30 11:57:41 +0200

    add Splitting

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
 import pygame
+import random
 from constants import *
+from logger import log_event
 from player import *
 from circleshape import CircleShape
 
@@ -17,6 +19,31 @@ class Asteroid(CircleShape):
             self.radius,
             LINE_WIDTH,
         )
+
+    def split(self):
+        # This asteroid is destroyed immediately
+        self.kill()
+
+        # If the radius of the asteroid is less than or equal to ASTEROID_MIN_RADIUS, just return; this was a small asteroid and we're done.
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+
+        # Otherwise, we need to spawn 2 new asteroids like so:
+        log_event("asteroid_split")
+
+        # random rotation 20..50 degrees
+        angle = random.uniform(20, 50)
+
+        first_velocity = self.velocity.rotate(angle) * 1.2
+        second_velocity = self.velocity.rotate(-angle) * 1.2
+
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+        asteroid_1 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid_2 = Asteroid(self.position.x, self.position.y, new_radius)
+
+        asteroid_1.velocity = first_velocity
+        asteroid_2.velocity = second_velocity
 
     def update(self, dt):
         # move in a straight line at constant velocity
